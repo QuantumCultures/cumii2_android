f583f6dfd6072d6ac476a10bc1b2d3f4329fa494
iamsweelai <iamsweelai@gmail.com>
2017-10-27 11:13:36 +0800

    Change Config to FPIS server

--- app/src/main/java/sg/lifecare/cumii/CumiiConfig.java
+++ app/src/main/java/sg/lifecare/cumii/CumiiConfig.java
@@ -4,18 +4,19 @@ public class CumiiConfig {
 
     //public static final String SERVER_URL = "https://www.lifecare.sg";
 
-    //public static final String SERVER_URL = "http://astralink.hansap.com";
-    //public static final String TEST_USERNAME = "tester+c1@astralink.com.sg";
-    //public static final String TEST_PASSWORD = "astralink123";
+    public static final String SERVER_URL = "http://astralink.hansap.com";
+    public static final String TEST_USERNAME = "tester+c1@astralink.com.sg";
+    public static final String TEST_PASSWORD = "astralink123";
 
-    public static final String SERVER_URL = "http://atthings.us-east-1.elasticbeanstalk.com";
-    public static final String TEST_USERNAME = "tester@astralink.com.sg";
-    public static final String TEST_PASSWORD = "astralink1234";
+    //public static final String SERVER_URL = "http://atthings.us-east-1.elasticbeanstalk.com";
+    //public static final String TEST_USERNAME = "tester@astralink.com.sg";
+    //public static final String TEST_PASSWORD = "astralink1234";
 
     //public static final String SERVER_URL = "https://www.lifecare.sg";
     //public static final String TEST_USERNAME = "tester@astralink.com.sg";
     //public static final String TEST_PASSWORD = "astralink1234";
 
-    public static final String MQTT_URL = "tcp://atthings.us-east-1.elasticbeanstalk.com:3600";
+    //public static final String MQTT_URL = "tcp://atthings.us-east-1.elasticbeanstalk.com:3600";
     //public static final String MQTT_URL = "tcp://broker.hivemq.com:1883";
+    public static final String MQTT_URL = "tcp://61.183.247.113:3600";
 }
